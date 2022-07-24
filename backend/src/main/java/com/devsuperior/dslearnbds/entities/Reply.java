c86162f0b5fa0b386d240bb012be0529d5c2291d
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-07-24 20:36:53 -0300

    Repositories.

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Reply.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Reply.java
@@ -2,13 +2,10 @@ package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -16,67 +13,45 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
-@Entity
-@Table(name = "tb_topic")
-public class Topic implements Serializable {
+public class Reply implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String title;
-	
 	
 	@Column(columnDefinition = "TEXT")
 	private String body;
-	
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
 	
 	@ManyToOne
+	@JoinColumn(name = "topic_id")
+	private Topic topic;
+	
+	@ManyToOne
 	@JoinColumn(name = "author_id")
 	private User author;
 	
-	@ManyToOne
-	@JoinColumn(name = "offer_id")
-	private Offer offer;
-	
-	
-	@ManyToOne
-	@JoinColumn(name = "lesson_id")
-	private Lesson lesson;
-	
-	@ManyToOne
-	@JoinColumn(name = "reply_id")
-	private Reply answer;
-	
 	@ManyToMany
-	@JoinTable(name = "tb_topic_likes",
-				joinColumns = @JoinColumn(name = "topic_id"),
+	@JoinTable(name = "tb_reply_likes",
+				joinColumns = @JoinColumn(name = "reply_id"),
 				inverseJoinColumns = @JoinColumn(name = "user_id"))
-	private Set<User> likes = new HashSet<>();
+	private Set<User>likes = new HashSet<>();
 	
-	@OneToMany(mappedBy = "topic")
-	private List<Reply> replies = new ArrayList<>();
-	
-	public Topic() {
+	public Reply() {
 		
 	}
 
-	public Topic(Long id, String title, String body, Instant moment, User author, Offer offer, Lesson lesson,
-			Set<User> likes) {
+	public Reply(Long id, String body, Instant moment, Topic topic, User author, Set<User> likes) {
 		super();
 		this.id = id;
-		this.title = title;
 		this.body = body;
 		this.moment = moment;
+		this.topic = topic;
 		this.author = author;
-		this.offer = offer;
-		this.lesson = lesson;
 		this.likes = likes;
 	}
 
@@ -86,14 +61,6 @@ public class Topic implements Serializable {
 
 	public void setId(Long id) {
 		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
 	}
 
 	public String getBody() {
@@ -112,28 +79,20 @@ public class Topic implements Serializable {
 		this.moment = moment;
 	}
 
+	public Topic getTopic() {
+		return topic;
+	}
+
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+
 	public User getAuthor() {
 		return author;
 	}
 
 	public void setAuthor(User author) {
 		this.author = author;
-	}
-
-	public Offer getOffer() {
-		return offer;
-	}
-
-	public void setOffer(Offer offer) {
-		this.offer = offer;
-	}
-
-	public Lesson getLesson() {
-		return lesson;
-	}
-
-	public void setLesson(Lesson lesson) {
-		this.lesson = lesson;
 	}
 
 	public Set<User> getLikes() {
@@ -144,21 +103,6 @@ public class Topic implements Serializable {
 		this.likes = likes;
 	}
 
-	
-	
-	public Reply getAnswer() {
-		return answer;
-	}
-
-	public void setAnswer(Reply answer) {
-		this.answer = answer;
-	}
-
-	public List<Reply> getReplies() {
-		return replies;
-	}
-
-	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -175,7 +119,7 @@ public class Topic implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Topic other = (Topic) obj;
+		Reply other = (Reply) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -183,6 +127,7 @@ public class Topic implements Serializable {
 			return false;
 		return true;
 	}
+	
 	
 	
 
